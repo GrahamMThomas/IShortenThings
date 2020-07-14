6964ca12100cd363104862dea58408f9f7ba1e28
GrahamMThomas <grahamthethomas@gmail.com>
2020-07-14 14:00:13 -0700

    Adds is_url validation with test

---
+++ back-end/tests/unit/test_validations.py
@@ -0,0 +1,12 @@
+from my_app import validations
+
+
+def test_is_url_validator():
+    assert validations.is_url("https://api.google.com")
+    assert validations.is_url("http://google.com")
+
+    assert not validations.is_url("http://google")
+    assert not validations.is_url("google.com")
+    assert not validations.is_url("google")
+    assert not validations.is_url("")
+    assert not validations.is_url(None)
