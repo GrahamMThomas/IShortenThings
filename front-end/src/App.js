a2b9d1f7d08bea4af6d934d5081ee9d55caee275
GrahamMThomas <grahamthethomas@gmail.com>
2020-07-15 11:52:37 -0700

    Updates some css; Adds background image

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { TextField, Button, Slider, Typography, Grid } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles'
 
 const styles = theme => ({
   title: {
-    fontFamily: 'avengers'
+    fontFamily: 'avengers',
+    fontSize: '4em'
   }
 })
 
 const App = (props) => {
   const { classes } = props;
+  const [newRedirect, setNewRedirect] = useState(null)
 
   return (
     <div className="App">
@@ -18,6 +20,7 @@ const App = (props) => {
         <Typography className={classes.title}> I Shorten Things</Typography>
         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
         <Button variant="contained" color="primary">Shorten Me!</Button>
+        <Typography>{newRedirect || ""}</Typography>
       </header>
     </div >
   );
