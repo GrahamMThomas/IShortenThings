cb7392a7a5723aa7c66327d2617d1a73c4c7cc59
GrahamMThomas <grahamthethomas@gmail.com>
2020-07-14 23:29:34 -0700

    Adds title font and material ui package

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { TextField, Button, Slider, Typography, Grid } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles'
 
-function App() {
+const styles = theme => ({
+  title: {
+    fontFamily: 'avengers'
+  }
+})
+
+const App = (props) => {
+  const { classes } = props;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography className={classes.title}> I Shorten Things</Typography>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <Button variant="contained" color="primary">Shorten Me!</Button>
       </header>
-    </div>
+    </div >
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
