e2a6c0a05c6b0cc2cfc1fdda3bae4b50febe6e7d
GrahamMThomas <grahamthethomas@gmail.com>
2020-07-17 15:03:12 -0700

    Matches front and back end regex

--- front-end/src/routes/components/RedirectForm/validations.js
+++ front-end/src/routes/components/RedirectForm/validations.js
@@ -1,7 +1,7 @@
 // https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
 function validURL(str) {
   var pattern = new RegExp(
-    "^(https?:\\/\\/)?" + // protocol
+    "^(https?:\\/\\/)" + // protocol
     "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
     "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
     "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
