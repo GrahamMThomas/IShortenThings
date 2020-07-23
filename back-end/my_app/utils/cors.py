8ba594311aed86828d6f65083baba933f05f767d
GrahamMThomas <grahamthethomas@gmail.com>
2020-07-23 12:48:25 -0700

    Adds CORS config

--- back-end/my_app/utils/cors.py
+++ back-end/my_app/utils/cors.py
@@ -4,12 +4,22 @@ import json
 
 def add_cors(func):
     def wrapper(*args, **kwargs):
+        allowed_domains = [
+            "https://master.d2t1c4oka9hdou.amplifyapp.com",
+            "https://app.ishortenthings.com",
+            "http://localhost:3000",
+        ]
 
+        origin = args[0].get("headers", {}).get("Origin")
         output = func(*args, **kwargs)
         if not output.get("headers"):
             output["headers"] = {}
-        output["headers"]["Access-Control-Allow-Origin"] = "*"
-        output["headers"]["Access-Control-Allow-Methods"] = "*"
+        if origin in allowed_domains:
+            output["headers"]["Access-Control-Allow-Origin"] = origin
+        else:
+            output["headers"]["Access-Control-Allow-Origin"] = None
+
+        output["headers"]["Access-Control-Allow-Methods"] = "POST,GET,OPTIONS"
         output["headers"]["Access-Control-Allow-Headers"] = "*"
         return output
 
